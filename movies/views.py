724a15ced75e8675bb2e9ccb799e5073f6e9ad75
Eduardo Margulis <edumarg@gmx.com>
2021-03-21 16:44:33 +0200

    added movie detail view

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Movie
 
 
@@ -7,3 +7,8 @@ from .models import Movie
 def index(request):
     movies = Movie.objects.all()
     return render(request, "movies/index.html", {'movies': movies})
+
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, "movies/details.html", {"movie": movie})
